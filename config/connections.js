6878d114131a6f8c7c61417883a4633b5d943934
CyberOne <pat@Patrices-MBP.fios-router.home>
2019-03-01 21:17:46 -0500

    fixed heroku connection issue

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-   port: 3306,
-   host: "localhost",
-  user: "root",
-   password: "Khym@ine3",
-  database: "burgers_db"
- });
+if(process.env.JAWSDB_URL){
+  connecton= mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else {
+
+    connection = mysql.createConnection ({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Khym@ine3",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
