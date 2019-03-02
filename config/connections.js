b63e87f8487895923c9f59a41da1cd8964d22368
CyberOne <pat@Patrices-MBP.fios-router.home>
2019-03-01 21:43:27 -0500

    heroku connection

--- config/connections.js
+++ config/connections.js
@@ -1,28 +1,24 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-  }
-  else {
-    connection = mysql.createConnection ({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "Khym@ine3",
-    database: "burgers_db"
-  });
-  }
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+        connection = mysql.createConnection({
+                        host: "localhost",
+                        user: "root",
+                        password: "Khym@ine3",
+                        port: "3306",
+                        database: "burgers_db"
+                    });
+         };
 
-// Make connection.
 connection.connect(function(err) {
-  if (err) {
-     console.error("error connecting: " + err.stack);
-     return;
-   }
-   console.log("connected as id " + connection.threadId);
- });
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-// Export connection for our ORM to use.
 module.exports = connection;
